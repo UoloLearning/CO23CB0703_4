ef71bc211fda8bf7fab09d0d1f6b268f61101ba5
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-24 18:19:36 +0530

    Update CO23CB0703_4_AA1.py

--- CO23CB0703_4_AA1.py
+++ CO23CB0703_4_AA1.py
@@ -1,1 +1,14 @@
+def recursive_function(n):
+    """This function returns multiple values using recursion."""
+    if n == 0:
+        return 0, 1  # base case
+    else:
+        a, b = recursive_function(n-1)
+        return b, a + b
 
+# Call the recursive_function with an argument of 5
+result1, result2 = recursive_function(5)
+
+# Print the results
+print("Result 1:", result1)
+print("Result 2:", result2)
