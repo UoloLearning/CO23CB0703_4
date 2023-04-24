b489fe8af1ac8bf1edf0e81c073d14775c4632dd
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-24 18:28:36 +0530

    Update CO23CB0703_4_AA3.py

--- CO23CB0703_4_AA3.py
+++ CO23CB0703_4_AA3.py
@@ -1,1 +1,12 @@
+def fibonacci_recursive(n):
+    """This function generates the Fibonacci sequence using recursion."""
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
 
+# Generate the first 10 terms of the Fibonacci sequence
+for i in range(10):
+    print(fibonacci_recursive(i))
